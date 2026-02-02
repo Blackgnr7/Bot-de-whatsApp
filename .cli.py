9ab32ea67ee2c035951651cac80cf27239589f6d
IyinG <165347923+Blackgnr7@users.noreply.github.com>
2026-02-02 14:03:32 -0300

    sorry

--- .cli.py
+++ .cli.py
@@ -7,11 +7,12 @@ driver.get("https://web.whatsapp.com")
 
 if __name__ == "__main__":
     input("Escaneia pf o QR_code e aperte Enter para continuar")
-    enviar_mensagem.mandar_mensagem_dentro_de_uma_conversa("oi, porra")
-    """ try:
+    try:
         while True:
             acho = ler_mensagem.ler_mensagem_quando_alguem_enviado()
             if acho:
                 enviar_mensagem.mandar_mensagem_dentro_de_uma_conversa("oi, ola tudo bem?")
     except KeyboardInterrupt:
-        print("Ctrl + C foi pressionado. Realizando limpeza...") """
+        print("Ctrl + C foi pressionado. Realizando limpeza...")
+
+""" file for test """
