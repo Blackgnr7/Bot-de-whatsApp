61f24b95c6177b6015a8fbdf3ee2cba978f80899
IyinG <165347923+Blackgnr7@users.noreply.github.com>
2026-02-02 14:01:14 -0300

    fix send a message

--- enviar_mensagem.py
+++ enviar_mensagem.py
@@ -9,18 +9,15 @@ import time
 df = pandas.read_csv("read.csv")
 
 
-def enviar_quandor_achar_button():
+def achar_button():
     driver = get_driver()
-    while True:
-        try:
-            elemento = driver.find_element(By.CSS_SELECTOR,'button[aria-label="Enviar"]')
-            break
-        except NoSuchElementException:
-            time.sleep(1)
+    wait = WebDriverWait(driver, 20)
+    elemento = wait.until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'button[aria-label="Enviar"]'))
+    )
     time.sleep(1)
     elemento.click()
     time.sleep(2)
-
 
 def enviar_pelo_arquivo_csv():
     driver = get_driver()
@@ -28,22 +25,22 @@ def enviar_pelo_arquivo_csv():
         driver.get(
             f"https://web.whatsapp.com/send/?phone={row.to_dict()['numero_de_telefone']}&text={row.to_dict()[' mensagem']}"
         )
-        enviar_quandor_achar_button()
+        achar_button()
 
 
-def qualquer_numero_quiser(telefone: str, mensagem: str):
+def mandar_mensagem_pelo_numero(telefone: str, mensagem: str):
     driver = get_driver()
     driver.get(f"https://web.whatsapp.com/send/?phone={telefone}&text={mensagem}")
     assert "WhatsApp" in driver.title
-    enviar_quandor_achar_button()
+    achar_button()
 
 
-def mandar_mensagem_dentro_de_uma_conversa(mensagem: str):
+def mandar_mensagem(mensagem: str):
     driver = get_driver()
     wait = WebDriverWait(driver, 20)
     while True:
         try:
-            elemento = driver.find_elements(By.CSS_SELECTOR,'p[dir="auto"]')
+            elemento = driver.find_elements(By.CSS_SELECTOR, 'p[dir="auto"]')
             break
         except NoSuchElementException:
             time.sleep(1)
@@ -52,17 +49,17 @@ def mandar_mensagem_dentro_de_uma_conversa(mensagem: str):
     elemento.click()
     elemento.send_keys(mensagem)
     time.sleep(2)
-    enviar_quandor_achar_button()
+    achar_button()
 
 
 def colocar_mensagem(mensagem: str):
     driver = get_driver()
-    while True:
-        try:
-            elemento = driver.find_elements(By.CSS_SELECTOR,'p[dir="auto"]')
-            break
-        except NoSuchElementException:
-            time.sleep(1)
+    wait = WebDriverWait(driver, 20)
+    elemento = wait.until(
+        EC.presence_of_all_elements_located(
+            (By.CSS_SELECTOR, 'p[dir="auto"]')
+        )
+    )
     elemento = elemento[1]
     time.sleep(1)
     elemento.click()
